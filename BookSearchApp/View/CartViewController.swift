fc786e9b04e80a0d54011713be8d2da9fb1532f0
heopill <heopill@naver.com>
2025-05-13 22:17:35 +0900

    [FEAT] CartViewController UI 구현 #4

--- BookSearchApp/View/CartViewController.swift
+++ BookSearchApp/View/CartViewController.swift
@@ -6,15 +6,133 @@
 //
 
 import UIKit
+import SnapKit
 
 class CartViewController: UIViewController {
-
+    
+    let data = [1, 2, 3]
+    
+    lazy var deleteAllButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체 삭제", for: .normal)
+        button.setTitleColor(.lightGray, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(deleteAllButtonTapped), for: .touchDown)
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "담은 책"
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        return label
+    }()
+    
+    lazy var addButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("추가", for: .normal)
+        button.setTitleColor(.systemGreen, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 15)
+        button.addTarget(self, action: #selector(addButtonTapped), for: .touchDown)
+        return button
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCompositionalLayout())
+        collectionView.register(ResultCollectionViewCell.self, forCellWithReuseIdentifier: ResultCollectionViewCell.identifier)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "DefaultCell")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = .systemPurple
+        
+        configureUI()
+    }
+    
+    private func configureUI() {
+        [deleteAllButton, titleLabel, addButton, collectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        deleteAllButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.leading.equalToSuperview().offset(20)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.centerX.equalToSuperview()
+        }
+        
+        addButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.trailing.equalToSuperview().inset(20)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    @objc private func deleteAllButtonTapped() {
+        print("전체 삭제 버튼 클릭")
+    }
+    
+    @objc private func addButtonTapped() {
+        print("추가 버튼 클릭")
+    }
+    
+    // CollectionView layout 만들기
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let layout = UICollectionViewCompositionalLayout { _, _ in
+            return self.createSectionOneLayout()
+        }
+        return layout
+    }
+    
+    // 섹션 레이아웃 만들기
+    private func createSectionOneLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(60))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 5, leading: 20, bottom: 0, trailing: 20)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(70))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
     }
     
     
+}
+
+
+extension CartViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: ResultCollectionViewCell.identifier, for: indexPath) as? ResultCollectionViewCell else {
+            return collectionView.dequeueReusableCell(withReuseIdentifier: "DefaultCell", for: indexPath)
+        }
+        
+        return cell2
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
     
 }
